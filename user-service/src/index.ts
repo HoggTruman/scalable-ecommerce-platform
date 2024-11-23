fdcf64d82f6e752c77d053dece07a1d902385921
Truman Hogg <hoggtruman@gmail.com>
2024-11-23 19:33:06 +0000

    Fix index.ts

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -6,20 +6,24 @@ dotenv.config();
 
 const dataSource = getDataSource();
 
-(async () => {
+async function main() {
     try {
         await dataSource.initialize();
         console.log("Data Source has been initialized!");
+
+        const Port = process.env.PORT;
+        if (Port === undefined) {
+            throw Error("PORT environment variable must be set.");
+        }
+
+        app.listen(Port, () => {
+            console.log(`Server is running at http://localhost:${Port}`);
+        });
     } catch (err) {
         throw err;
     }    
-})()
-
-const Port = process.env.PORT;
-if (Port === undefined) {
-    throw Error("PORT environment variable must be set.");
 }
 
-app.listen(Port, () => {
-    console.log(`Server is running at http://localhost:${Port}`);
-});
+
+main();
+
