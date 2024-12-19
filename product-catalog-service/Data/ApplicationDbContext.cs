f26125ff6bb631398476646415a05b74b841ef52
Truman Hogg <hoggtruman@gmail.com>
2024-12-19 01:47:35 +0000

    Add ApplicationDbContext

---
+++ product-catalog-service/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogService.Models;
+
+namespace ProductCatalogService.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
--- product-catalog-service/Program.cs
+++ product-catalog-service/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseNpgsql(@"Host=myserver;Username=postgres;Password=postgres;Database=product-catalog-service");
+});
+
+
+
 
 var app = builder.Build();
 
