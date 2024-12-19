1d219848eeef44f9f63659bc9265ab663ba194e8
Truman Hogg <hoggtruman@gmail.com>
2024-12-19 01:16:25 +0000

    Rename RootNamespace

--- product-catalog-service/Models/Product.cs
+++ product-catalog-service/Models/Product.cs
@@ -1,7 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace product_catalog_service.Models;
+namespace ProductCatalogService.Models;
+
 
 [Table("Product")]
 public class Product
