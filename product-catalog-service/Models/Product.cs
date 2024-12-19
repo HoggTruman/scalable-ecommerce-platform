5d1db8e0fb5ae597104a941d227a80078fccd264
Truman Hogg <hoggtruman@gmail.com>
2024-12-19 01:08:19 +0000

    Add Product model

---
+++ product-catalog-service/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace product_catalog_service.Models;
+
+[Table("Product")]
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required double Price { get; set; }
+    public required int StockCount { get; set; }
+}
