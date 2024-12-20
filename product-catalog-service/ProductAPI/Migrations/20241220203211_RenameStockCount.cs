f2da626ced835e4a004f2a1b8bb52fb66f7ba920
Truman Hogg <hoggtruman@gmail.com>
2024-12-20 20:33:46 +0000

    Rename Product stock count.
    Change StockCount to Stock.

---
+++ product-catalog-service/ProductAPI/Migrations/20241220203211_RenameStockCount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStockCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockCount",
+                table: "Product",
+                newName: "Stock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Stock",
+                table: "Product",
+                newName: "StockCount");
+        }
+    }
+}
