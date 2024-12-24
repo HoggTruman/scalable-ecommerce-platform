6e0ad16a77fc8caafc148808f5599c21d0333351
Truman Hogg <hoggtruman@gmail.com>
2024-12-24 00:14:48 +0000

    Update database context config

--- product-catalog-service/ProductAPI/Program.cs
+++ product-catalog-service/ProductAPI/Program.cs
@@ -1,18 +1,30 @@
+using dotenv.net;
 using Microsoft.EntityFrameworkCore;
 using ProductAPI.Data;
 using ProductAPI.Repositories;
 
+
+DotEnv.Load();
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseNpgsql(@"Host=myserver;Username=postgres;Password=postgres;Database=product-catalog-service");
+builder.Services.AddDbContextPool<ApplicationDbContext>(options => {
+    var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+    var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+    var db = Environment.GetEnvironmentVariable("POSTGRES_DATABASE");
+
+    options.UseNpgsql($@"Server=localhost;Port={port};Username=postgres;Password={password};Database={db};",
+        options => options
+            .SetPostgresVersion(17, 2)            
+    );
 });
 
 
